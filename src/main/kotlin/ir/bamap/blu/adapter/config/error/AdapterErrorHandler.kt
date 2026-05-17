048316a688946a14505f2e0afc5ffd55fe90ecda
Morteza Malvandi <malvandi.m@gmail.com>
2026-05-17 17:04:32 +0330

    Add AdapterErrorHandler

--- src/main/kotlin/ir/bamap/blu/adapter/config/error/AdapterErrorHandler.kt
+++ src/main/kotlin/ir/bamap/blu/adapter/config/error/AdapterErrorHandler.kt
@@ -8,11 +8,21 @@ import ir.bamap.blu.exception.BluException
 import ir.bamap.blu.exception.ExternalServiceException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.web.reactive.function.client.ClientRequest
+import org.springframework.web.reactive.function.client.ClientResponse
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction
+import org.springframework.web.reactive.function.client.ExchangeFunction
+import org.springframework.web.reactive.function.client.WebClientRequestException
+import org.springframework.web.reactive.function.client.bodyToMono
+import reactor.core.publisher.Mono
 import tools.jackson.core.JacksonException
 import tools.jackson.core.type.TypeReference
 import tools.jackson.databind.ObjectMapper
+import java.util.function.Function
+import java.util.function.Predicate
+import javax.naming.ServiceUnavailableException
 
-open class BluErrorDecoder(
+open class AdapterErrorHandler(
     protected val serviceName: String,
     protected val objectMapper: ObjectMapper
 ) {
@@ -23,7 +33,31 @@ open class BluErrorDecoder(
         initStrategies()
     }
 
-    fun decode(response: ResponseModel): BluException {
+    open fun handleResponseError(): ExchangeFilterFunction {
+        return ExchangeFilterFunction.ofResponseProcessor(Function { response: ClientResponse ->
+            if (response.statusCode().isError) {
+                return@Function response.bodyToMono<String>()
+                    .flatMap { body ->
+                        val request = response.request()
+                        val responseModel = ResponseModel(request, body, response.statusCode())
+                        Mono.error(decode(responseModel))
+                    }
+            }
+            Mono.just(response)
+        })
+    }
+
+    open fun handleConnectionError(): ExchangeFilterFunction {
+        return ExchangeFilterFunction { request: ClientRequest, next: ExchangeFunction ->
+            next.exchange(request)
+                .onErrorMap(
+                    Predicate { ex: Throwable? -> ex is WebClientRequestException },
+                    Function { ex: Throwable? -> ServiceUnavailableException(serviceName) }
+                )
+        }
+    }
+
+    protected open fun decode(response: ResponseModel): BluException {
         val jsonBody = convertBodyToMap(response.body)
         val jsonResponseModel = JsonResponseModel(jsonBody, response)
 
@@ -43,14 +77,13 @@ open class BluErrorDecoder(
 
         logger.atError()
             .setMessage("Error in External Service")
+            .addKeyValue("serviceName", serviceName)
             .addKeyValue("response", responseLog)
             .addKeyValue("request", requestLog)
             .log()
-
-
     }
 
-    protected fun convertBodyToMap(response: String): Map<String, Any?> {
+    protected open fun convertBodyToMap(response: String): Map<String, Any?> {
         try {
             return objectMapper.readValue(response, object : TypeReference<Map<String, Any?>>() {})
         } catch (e: JacksonException) {
@@ -63,7 +96,7 @@ open class BluErrorDecoder(
         }
     }
 
-    protected fun initStrategies() {
+    protected open fun initStrategies() {
         this.strategies.add(ByStatusErrorDecoderStrategy())
     }
 }
